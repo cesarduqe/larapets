f5d5a5820692a8fbeb2b85f2c4dc41e2e8d107a6
theflorian420 <diegoalejandroflorian07@gmail.com>
2026-07-02 20:08:43 -0500

    modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <section class="bg-white/60 p-4 rounded-md w-100 flex flex-col gap-4 justify-center items-center">
-    <img src="{{ asset('images/logo1.png') }}" class="size-70 object-contain" alt="LARAPETS Logo">
+    <img src="{{ asset('images/logo.jpeg') }}" class="size-70 object-contain" alt="LARAPETS Logo">
     <p class="text-justify text-[#215174]">
         Encuentra a tu compañero ideal y bríndale una segunda oportunidad.
     </p>
